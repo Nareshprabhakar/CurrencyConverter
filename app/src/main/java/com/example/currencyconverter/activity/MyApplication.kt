f9ab4e78d5a3a59e64c8f8d2a30ccb0a2847af06
naresh <nareshprabhakar162@gmail.com>
2022-03-27 18:48:50 +0530

    converter_currency

--- app/src/main/java/com/example/currencyconverter/activity/MyApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.example.currencyconverter.activity
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MyApplication: Application() {
-}
--- app/src/main/java/com/example/currencyconverter/model/Rates.kt
+++
@@ -1,9 +0,0 @@
-package com.example.currencyconverter.model
-
-import java.io.Serializable
-
-data class Rates (
-    val currency_name:String,
-    val rate:String,
-    val rate_for_amount:Double
-        ):Serializable
